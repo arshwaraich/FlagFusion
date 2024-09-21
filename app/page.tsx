19d50e119c507b79b48e652609f75af7d7f1ddce
Arshdeep Singh <arsh.waraich@icloud.com>
2024-09-21 05:45:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,26 @@ import Board from "@/components/Board/Board";
 import Dock, { Actions } from "@/components/Dock/Dock";
 import { useState } from "react";
 
-export const [FROM, TO] = ['IN', 'CA'];
+export const [FROM, TO] = ['SV', 'PE'];
 
 export default function Home() {
   const [from, setFrom] = useState<string>(FROM);
   const [color, setColor] = useState<string>();
+  const [reset, setReset] = useState<boolean>(false);
 
   return (
     <>
       <Board
         from={from}
+        reset={reset}
         color={color} />
       <Dock
         from={from}
         setFrom={setFrom}
         color={color}
         setColor={setColor} />
-      <Actions />
+      <Actions
+        resetFn={() => setReset((i) => !i)}/>
     </>
   );
 }
